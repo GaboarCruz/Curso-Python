144342361dbc9c304bffb25e5989247bc8a36179
GabrielArizaDeLaCruz <axercross@gmail.com>
2025-02-14 23:36:51 -0500

    Clase 5 enteros, flotantes y booleanos

---
+++ 5.int_float_bool.py
@@ -0,0 +1,20 @@
+# Los enteros son valores numericos enteros como dice su nombre.
+x = 10
+
+# Los decimales son valores numericos que tienen un punto decimal para expandir su valor.
+y = 5.2112
+#z = 1.2E4
+#a = 1.2E-4
+
+print(type(x));
+print(type(y));
+#print(z);
+print(x + x)
+print(x + y)
+print(y + y)
+
+is_true = True
+is_false = False
+
+print(type(is_true))
+print(type(is_false))

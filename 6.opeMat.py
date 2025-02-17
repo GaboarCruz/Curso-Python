cb7312e20377c528bc8bd8766c02eea4379d09c3
GabrielArizaDeLaCruz <86798784+GaboarCruz@users.noreply.github.com>
2025-02-17 18:27:45 +0000

    escritura en archivo 6.opeMat.py

---
+++ 6.opeMat.py
@@ -0,0 +1,15 @@
+x = 5 
+y = 10
+z = 4
+
+print(f"suma de {x} y {y}:", x + y)
+print(f"resta de {x} y {y}:", x - y)
+print(f"multiplicación de {x} y {y}:", x * y)
+print(f"división de {y} y {x}:", y / x)
+print(f"división de {y} y {z}:", y / z)
+print(f"division redondeada de {y} y {z}:", y//z)
+print(f"potencia de {x} al cuadrado", x**2)
+print(f"{x} > {z}", x > z)
+print(f"{x} < {z}", x < z)
+print(f"{x} == {z}", x == z)
+print(f"{x} != {z}", x != z)

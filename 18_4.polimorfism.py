5be5ae4e1ff9b864f2689490b9cd1eaa00f1953d
gaboarCruz <axercross@gmail.com>
2025-02-20 16:24:37 -0500

    Polimorfismo.py

--- 18_4.polimorfism.py
+++ 18_4.polimorfism.py
@@ -74,3 +74,62 @@ class Truck(Vehicle):
             return f"El motor del camión {self.brand} se ha detenido"
         else:
             return f"El camión {self.brand} No esta disponible"
+        
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchase_vehicles = []
+
+    def buy_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchase_vehicles.append(vehicle)
+        else:
+            print(f"Lo siento, {vehicle.brand} no está disponible")
+
+    def inquire_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available:
+            availability = "Disponible"
+        else:
+            availability = "No disponible"
+        print(f"El {vehicle.brand} esta {availability} y cuesta {vehicle.price}")
+
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+
+    def add_vehicles(self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print(f"El {vehicle.brand} ah sido añadido al inventario")
+
+    def add_customers(self, customer: Customer):
+        self.customers.append(customer)
+        print(f"El {customer.name} ah sido añadido")
+
+    def show_available_vehicles(self):
+        for vehicle in self.inventory:
+            if vehicle.check_available():
+                print(f"-{vehicle.brand} por {vehicle.get_price()}")
+
+
+car1 = Car("Toyota", "Corolla", 20000)
+bike1 = Bike("Yamaha", "MT-07", 7000)
+truck1 = Truck("Volvo", "FH16", 80000)
+
+customer1 = Customer("Carlos")
+
+dealership = Dealership()
+dealership.add_vehicles(car1)
+dealership.add_vehicles(bike1)
+dealership.add_vehicles(truck1)
+
+# Muestra todos los vehiculos
+dealership.show_available_vehicles()
+
+# Ve disponibilidad del carro
+customer1.inquire_vehicle(car1)
+
+# Compra el carro
+customer1.buy_vehicle(car1)
